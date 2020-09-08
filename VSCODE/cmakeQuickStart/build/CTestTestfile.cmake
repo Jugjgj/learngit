87cc5ccd9eb778be6037029a8539cb18ec812b5f
Jugjgj <jesaxi27@gmail.com>
2020-09-09 02:24:49 +0800

    start learning Cmake

---
+++ VSCODE/cmakeQuickStart/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/new/Repository/VSCODE/cmakeQuickStart
+# Build directory: /mnt/new/Repository/VSCODE/cmakeQuickStart/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
